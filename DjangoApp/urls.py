ac7b5b7be3593f4fdd734258fb62dc55a033a00c
Dave Allen <spondicious@me.com>
2023-09-08 12:37:39 +0200

    more adjustments

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -1,7 +1,7 @@
-"""DjangoApp URL Configuration
+"""djangowebsite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from users import views as user_views
+from competitions import views as comp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('other/', include('other.urls'))
-]
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('', include('main.urls')),
+    path('', include('competitions.urls')),
+    path('profile/', user_views.profile, name='profile'),
+    path('update/', user_views.update, name='update'),
+    path('comp_detail/', comp_views.comp_detail, name='comp-detail'),
+    path('currentcomps/', comp_views.currentcomps, name='current-comps'),
+    path('related/', comp_views.related, name='related'),
+    path('winners/', comp_views.winners, name='winners'),
+] # + static(settings.STATIC_URL, document)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
